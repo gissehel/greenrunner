d875fe1d04d6903d6a8cee8590f715f9000bb8f6
Gissehel <Gissehel@localhost>
2010-12-05 19:49:59 +0100

    Setup posix + windows

--- setup-win.py
+++ setup-win.py
@@ -1,11 +1,19 @@
 #!/usr/bin/env python
 from distutils.core import setup
+from babel.messages import frontend as babel
 import sys
+import py2exe
+from setup import setup_args, setup
+# import os
+# import yaml
 
 DIST_PATH = r'bin'
 
-sys.argv = [sys.argv[0],]
-sys.argv += ['py2exe','--dist-dir',DIST_PATH]
+if len(sys.argv) == 1 :
+    sys.argv = [sys.argv[0],]
+    sys.argv += ['py2exe','--dist-dir',DIST_PATH]
 
-import setup
+setup_args['console'] = ['greenrunner.py']
 
+if __name__=='__main__' :
+    setup(**setup_args)
--- setup.py
+++ setup.py
@@ -5,27 +5,31 @@ import sys
 # import os
 # import yaml
 
-setup(
-    # console=['greenrunner.py'],
-    name='greenrunner',
-    version='0.99',
-    description='Run a set of green paper page alredy included in a greenpepper labels page, and generate a report that can be archived',
-    url='http://code.google.com/p/greenrunner/',
-    author='Gissehel',
-    author_email='dist-greenrunner@gissehel.org',
-    scripts=['greenrunner.py'],
-    packages=['web','webrip','greenrunnerlib'],
-    options={
+setup_args = {
+    'name':'greenrunner',
+    'version':'0.99',
+    'description':'Run a set of green paper page alredy included in a greenpepper labels page, and generate a report that can be archived',
+    'url':'http://code.google.com/p/greenrunner/',
+    'author':'Gissehel',
+    'author_email':'dist-greenrunner@gissehel.org',
+    'scripts':['greenrunner.py'],
+    'packages':['web','webrip','greenrunnerlib'],
+    'options':{
         },
-    cmdclass = {'compile_catalog': babel.compile_catalog,
-                'extract_messages': babel.extract_messages,
-                'init_catalog': babel.init_catalog,
-                'update_catalog': babel.update_catalog},
-    message_extractors = {
+    'cmdclass' : {
+        'compile_catalog': babel.compile_catalog,
+        'extract_messages': babel.extract_messages,
+        'init_catalog': babel.init_catalog,
+        'update_catalog': babel.update_catalog,
+        },
+    'message_extractors' : {
         '': [
-            ('**.py',                'python', None),
+                ('**.py', 'python', None),
             ],
         },
-    data_files=[('locale/fr_FR/LC_MESSAGES', ['locale/fr_FR/LC_MESSAGES/greenrunner.mo'])],
+    'data_files' : [('locale/fr_FR/LC_MESSAGES', ['locale/fr_FR/LC_MESSAGES/greenrunner.mo'])],
+    }
 
-    )
+if __name__=='__main__' :
+    setup(**setup_args)
+
